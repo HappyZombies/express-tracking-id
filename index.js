431d32356e353e28ed5dfd95ff5aef24acd77c57
HappyZombies <daniel.reguero@hotmail.com>
2021-10-06 10:51:43 -0400

    Some small cleans up for real

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // credit: https://stackoverflow.com/a/2117523
 const uuidv4 = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
